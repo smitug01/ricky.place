5115ecc8dfdf6eb29acd2dbf7cb7a9696af47465
smitug01 <sumily20@gmail.com>
2023-10-19 14:35:24 +0800

    Initial commit

---
+++ app.config.ts
@@ -0,0 +1,26 @@
+export default defineAppConfig({
+  docus: {
+    title: "Ricky's Home",
+    description: '只要努力，平凡的人也能走出不平凡的路、跳脫平凡。',
+    image: 'https://nrix.org/img/static-image.png',
+    socials: {
+      github: 'smitug01',
+      linkedin: 'smitug01',
+    },
+    aside: {
+      level: 0,
+      exclude: []
+    },
+    header: {
+      logo: true
+    },
+    footer: {
+      textLinks: [
+        {
+          text: '2021 - 2023 Cheng-Hung Lu',
+          href: 'https://ricky.place'
+        }
+      ]
+    }
+  }
+})
---
+++ components/Logo.vue
@@ -0,0 +1,17 @@
+<template>
+  <span style="font-weight: 600; font-size: 1.4rem; line-height: 1.75rem;">
+    Ricky's Home
+  </span>
+</template>
+
+<style lang="ts" scoped>
+    css({
+    svg: {
+        color: '{color.gray.900}',
+        height: 'inherit',
+        '@dark': {
+        color: '{color.gray.100}'
+        },
+    }
+    })
+</style>
